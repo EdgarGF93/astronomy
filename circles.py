a621e3b83a2c0df79e91951dbdfe68ca10fef939
Edgar Gutierrez <edgargutierrezfdez@gmail.com>
2023-08-08 11:34:01 +0200

    sun methods

--- circles.py
+++ circles.py
@@ -3,21 +3,49 @@ import matplotlib.pyplot as plt
 
 def draw_earth():
     fig, axes = plt.subplots()
-    earth = plt.Circle((0, 0.), EARTH_RADIUS_SCALED, color=EARTH_COLOR)
+    earth = plt.Circle((0, 0.), EARTH_RADIUS_SCALED, color=np.array(EARTH_COLOR) / 255)
     axes.set_aspect(1)
     axes.add_artist(earth)
     plt.title('EARTH')
-    plt.xlim([-2,2])
-    plt.ylim([-2,2])
+    plt.xlim([-EARTH_RADIUS_SCALED*2,EARTH_RADIUS_SCALED*2])
+    plt.ylim([-EARTH_RADIUS_SCALED*2,EARTH_RADIUS_SCALED*2])
     plt.show()
             
 
 def draw_moon():
     fig, axes = plt.subplots()
-    moon = plt.Circle((0, 0.), MOON_RADIUS_SCALED, color=MOON_COLOR)
+    moon = plt.Circle((0, 0.), MOON_RADIUS_SCALED, color=np.array(MOON_COLOR) / 255)
     axes.set_aspect(1)
     axes.add_artist(moon)
     plt.title('MOON')
-    plt.xlim([-2,2])
-    plt.ylim([-2,2])
+    plt.xlim([-MOON_RADIUS_SCALED*2,MOON_RADIUS_SCALED*2])
+    plt.ylim([-MOON_RADIUS_SCALED*2,MOON_RADIUS_SCALED*2])
+    plt.show()
+
+def draw_sun():
+    fig, axes = plt.subplots()
+    sun = plt.Circle((0, 0.), SUN_RADIUS_SCALED, color=np.array(SUN_COLOR) / 255)
+    axes.set_aspect(1)
+    axes.add_artist(sun)
+    plt.title('SUN')
+    plt.xlim([-SUN_RADIUS_SCALED*2,SUN_RADIUS_SCALED*2])
+    plt.ylim([-SUN_RADIUS_SCALED*2,SUN_RADIUS_SCALED*2])
+    plt.show()
+
+
+def draw_solar_system():
+    fig, axes = plt.subplots()
+
+    sun = plt.Circle((0, 0.), SUN_RADIUS_SCALED, color=np.array(SUN_COLOR) / 255)
+    earth = plt.Circle((SUN_EARTH_DISTANCE_SCALED, 0.), EARTH_RADIUS_SCALED, color=np.array(EARTH_COLOR) / 255)
+    moon = plt.Circle((SUN_EARTH_DISTANCE_SCALED + EARTH_MOON_DISTANCE_SCALED, 0.), MOON_RADIUS_SCALED, color=np.array(MOON_COLOR) / 255)
+
+    axes.add_artist(sun)
+    axes.add_artist(earth)
+    # axes.add_artist(moon)
+    axes.set_aspect(1)
+
+    plt.xlim([-SUN_RADIUS_SCALED*2,SUN_EARTH_DISTANCE_SCALED*2])
+    plt.ylim([-SUN_RADIUS_SCALED*2,SUN_RADIUS_SCALED*2])
+
     plt.show()
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 
 
-from circles import draw_earth, draw_moon
+from circles import draw_earth, draw_moon, draw_sun, draw_solar_system
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def kk():
+    fig, ax = plt.subplots(figsize=(6,6))
+    mat = np.array([
+        [(50,50,50)]
+    ]).astype(int)
+
+    plt.imshow(mat)
+    plt.show()
+
 
 
 
 if __name__ == '__main__':
-    draw_moon()
+    draw_solar_system()
+
